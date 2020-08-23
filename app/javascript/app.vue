dfd600ed9c96c23cb87a9696db5a917bda76df14
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2020-08-23 13:43:33 -0500

    Initial commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <DxSelectBox></DxSelectBox>
+  </div>
+</template>
+
+<script>
+  import DxSelectBox from 'devextreme-vue/select-box'
+  export default {
+    components: {
+      DxSelectBox
+    },
+    data: function () {
+      return {
+        message: "Hello Vue!"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
